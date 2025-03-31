151b385d47e15f0bbcc956ed537b69523a6192b1
jamerson-mt <jjamersonmt@gmail.com>
2025-03-31 10:32:40 -0300

    nao conseguir ainda atualizar para a troca do docente da disciplina

--- src/components/Disciplinas/EditarDisciplina.vue
+++ src/components/Disciplinas/EditarDisciplina.vue
@@ -15,7 +15,8 @@ const disciplina = ref({
   cargaHoraria: 0
 });
 
-const cursos = ref([])
+const cursos = ref([]);
+const docentes = ref([]);
 
 const mensagem = ref('');
 const tipoMensagem = ref('');
@@ -65,11 +66,6 @@ const atualizarDisciplina = () => {
   }
 };
 
-const trocarDocente = () => {
-  // Lógica para trocar o docente atual
-  console.log('Trocar docente acionado');
-};
-
 const fetchCursos = async () => {
   try {
     const response = await fetch("http://localhost:5117/api/curso");
@@ -77,6 +73,16 @@ const fetchCursos = async () => {
     cursos.value = data;
   } catch (error) {
     console.error("Erro ao buscar cursos:", error);
+  }
+};
+
+const fetchDocentes = async () => {
+  try {
+    const response = await fetch("http://localhost:5117/api/docente");
+    const data = await response.json();
+    docentes.value = data;
+  } catch (error) {
+    console.error("Erro ao buscar docentes:", error);
   }
 };
 
@@ -94,6 +100,7 @@ const fetchDisciplina = async () => {
 
 onMounted(() => {
   fetchCursos();
+  fetchDocentes();
   if (route.params.id) {
     disciplina.value.id = route.params.id;
     fetchDisciplina();
@@ -109,124 +116,133 @@ const voltar = () => {
     <button @click="voltar">Voltar</button>
     <h1>Editar Disciplina</h1>
     <div v-if="mensagem" :class="tipoMensagem">{{ mensagem }}</div>
-    <form @submit.prevent="atualizarDisciplina">
-      <div>
-        <label for="name">Nome da disciplina:</label>
-        <input type="text" id="name" v-model="disciplina.name" required />
-      </div>
-      <div>
-        <label for="cursoId">Curso da disciplina:</label>
-        <select name="cursoId" id="cursoId" v-model="disciplina.cursoId" class="styled-select">
-          <option v-for="curso in cursos" :key="curso.id" :value="curso.id">
-            {{ curso.name }}
-          </option>
-        </select>
-      </div>
-      <div>
-        <label for="periodo">Periodo da disciplina:</label>
-        <input type="range" id="periodo" v-model="disciplina.periodo" min="1" max="10" />
-        <span>{{ disciplina.periodo }}</span>
-      </div>
-      <div>
-        <label for="cargaHoraria">Carga horária da disciplina:</label>
-        <input type="range" id="cargaHoraria" v-model="disciplina.cargaHoraria" min="1" max="100" />
-        <span>{{ disciplina.cargaHoraria }} horas</span>
-      </div>
-      <button type="submit">Atualizar</button>
-    </form>
-    <button @click="trocarDocente" class="trocar-docente">Trocar Docente</button>
+    <div class="container-editar-disciplina-content">
+      <form @submit.prevent="atualizarDisciplina">
+        <div>
+          <label for="name">Nome da disciplina:</label>
+          <input type="text" id="name" v-model="disciplina.name" required />
+        </div>
+        <div>
+          <label for="cursoId">Curso da disciplina:</label>
+          <select name="cursoId" id="cursoId" v-model="disciplina.cursoId" class="styled-select">
+            <option v-for="curso in cursos" :key="curso.id" :value="curso.id">
+              {{ curso.name }}
+            </option>
+          </select>
+        </div>
+        <div>
+          <label for="periodo">Periodo da disciplina:</label>
+          <input type="range" id="periodo" v-model="disciplina.periodo" min="1" max="10" />
+          <span>{{ disciplina.periodo }}</span>
+        </div>
+        <div>
+          <label for="cargaHoraria">Carga horária da disciplina:</label>
+          <input type="range" id="cargaHoraria" v-model="disciplina.cargaHoraria" min="1" max="100" />
+          <span>{{ disciplina.cargaHoraria }} horas</span>
+        </div>
+        <button type="submit">Atualizar</button>
+      </form>
+    </div>
   </div>
 </template>
 
 
 <style scoped>
 .container-editar-disciplina {
-  grid-column: 1 / -1; /* Faz o contêiner ocupar todas as colunas */
-  justify-self: center; /* Centraliza horizontalmente */
-  align-self: center; /* Centraliza verticalmente */
   display: flex;
   flex-direction: column;
+  justify-content: center;
+  align-items: center;
   background-color: #f9f9f9;
-  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-  border-radius: 10px;
-  padding: 2rem 4rem;
-  width: 30rem;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+  border-radius: 15px;
+  padding: 4rem;
+  width: 90%;
+  max-width: 800px;
+  height: 90vh;
+  margin: 30px auto;
+  overflow-y: auto;
 }
 
-.container-editar-disciplina button {
-  align-self: self-start;
-  background-color: #127247;
-  padding: 0.4rem 1rem;
-  border-radius: 50px;
-  border: none;
-  color: #ffffff;
-  font-size: 14pt;
-  font-weight: 600;
-  cursor: pointer;
+.container-editar-disciplina-content {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  width: 100%;
+  gap: 2rem;
 }
 
 .container-editar-disciplina h1 {
   text-align: center;
-  margin-bottom: 20px;
-  font-weight: 500;
-  margin-top: 1rem;
+  margin-bottom: 1.5rem;
+  font-weight: 600;
+  font-size: 24pt;
+  color: #333;
+}
+
+.container-editar-disciplina form {
+  display: flex;
+  flex-direction: column;
+  width: 60%;
+}
+
+.container-trocar-docente {
+  display: flex;
+  flex-direction: column;
+  width: 35%;
 }
 
 .container-editar-disciplina form div {
-  margin-bottom: 15px;
+  margin-bottom: 20px;
 }
 
 .container-editar-disciplina form label {
   display: block;
-  margin-bottom: 5px;
+  margin-bottom: 8px;
   font-weight: 500;
   font-size: 14pt;
+  color: #555;
 }
 
-.container-editar-disciplina form input {
-  width: 100%;
-  padding: 8px;
+.container-editar-disciplina form input,
+.container-editar-disciplina form select {
+  padding: 12px;
   box-sizing: border-box;
   border: 1px solid #ccc;
-  border-radius: 10px;
+  border-radius: 8px;
   font-size: 14pt;
+  background-color: #fff;
+  transition: border-color 0.3s ease;
+}
+
+.container-editar-disciplina form input:focus,
+.container-editar-disciplina form select:focus {
+  border-color: #007bff;
+  outline: none;
 }
 
 .container-editar-disciplina form input[type="range"] {
-  width: 100%;
-  margin: 5px 0;
+  margin: 10px 0;
 }
 
 .container-editar-disciplina form .styled-select {
-  width: 100%;
-  padding: 8px;
-  box-sizing: border-box;
-  border: 1px solid #ccc;
-  border-radius: 10px;
-  font-size: 14pt;
-  background-color: #f9f9f9;
   appearance: none;
   -webkit-appearance: none;
   -moz-appearance: none;
 }
 
-.container-editar-disciplina form .styled-select:focus {
-  border-color: #007bff;
-  outline: none;
-}
-
-.container-editar-disciplina form .styled-select option {
-  font-size: 14pt;
-}
-
 .container-editar-disciplina form button {
-  width: 100%;
-  padding: 10px;
+  margin-top: 1rem;
+  width: 30%;
+  padding: 12px;
   background-color: #007bff;
   color: white;
   border: none;
-  border-radius: 100px;
+  border-radius: 50px;
+  font-size: 12pt;
+  font-weight: 600;
   cursor: pointer;
+  transition: background-color 0.3s ease;
 }
 
 .container-editar-disciplina form button:hover {
@@ -236,26 +252,45 @@ const voltar = () => {
 .container-editar-disciplina .trocar-docente {
   margin-top: 1rem;
   background-color: #ff9800;
-  padding: 0.4rem 1rem;
+  padding: 0.6rem 1.2rem;
   border-radius: 50px;
   border: none;
   color: #ffffff;
   font-size: 14pt;
   font-weight: 600;
   cursor: pointer;
+  transition: background-color 0.3s ease;
 }
 
 .container-editar-disciplina .trocar-docente:hover {
   background-color: #e68900;
 }
 
+.container-trocar-docente select {
+  padding: 12px;
+  box-sizing: border-box;
+  border: 1px solid #ccc;
+  border-radius: 8px;
+  font-size: 14pt;
+  background-color: #fff;
+  transition: border-color 0.3s ease;
+  width: 100%;
+}
+
+.container-trocar-docente select:focus {
+  border-color: #007bff;
+  outline: none;
+}
+
 .sucesso {
   color: green;
   margin-bottom: 1rem;
+  font-weight: 500;
 }
 
 .erro {
   color: red;
   margin-bottom: 1rem;
+  font-weight: 500;
 }
 </style>
